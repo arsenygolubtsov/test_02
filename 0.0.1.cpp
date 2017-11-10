563f7c589410e7a9f4156d99497accef5e886b39
arsenygolubtsov <32494075+arsenygolubtsov@users.noreply.github.com>
2017-11-10 11:22:36 +0300

    Update 0.0.1.cpp

--- 0.0.1.cpp
+++ 0.0.1.cpp
@@ -10,31 +10,37 @@ void reverse(double * & array, unsigned int size) {
 }
 
 bool read(double *array, unsigned int size){
-	string stroka;
-	getline(cin, stroka);
-	istringstream stream(stroka);
-	for (unsigned int i = 0; i<size; ++i){
-		if (!(stream >> array[i])){
-			cout << "error" << endl;
-                return false;
-		}
-	}
-	return true;
+    char x;
+    string stroka;
+    getline(cin, stroka);
+    istringstream stream(stroka);
+    for (unsigned int i = 0; i<size; ++i){
+        if (!(stream >> array[i])){
+            cout << "error" << endl;
+            return false;
+
+        }
+    }
+    if (stream>>x) {
+        cout << "An error has occured while reading input data.";
+        exit(0);
+    }
+    return true;
 }
 
 int main(){
-	unsigned int size;
-	cin >> size;
-	cin.get();
-	double *array = new double[size];
-	if (read(array, size)){
-		reverse(array, size);
-		for (unsigned int i = 0; i<size; ++i)
-		cout << array[i] << " ";
-	}
-	else {
-		cout << "An error has occured while reading input data.";
-	}
-	delete[]array;
-	return 0;
+    unsigned int size;
+    cin >> size;
+    cin.get();
+    double *array = new double[size];
+    if (read(array, size)){
+        reverse(array, size);
+        for (unsigned int i = 0; i<size; ++i)
+            cout << array[i] << " ";
+    }
+    else {
+        cout << "An error has occured while reading input data.";
+    }
+    delete[]array;
+    return 0;
 }
